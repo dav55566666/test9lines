addae1f740a5e7562486d9b2bb593b66d6e7ba4a
unknown <hovsepyandavid0@gmail.com>
2021-10-25 20:18:25 +0400

    test

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import './vendor';
+import './helpers';
+import './components/social';
+import {ieFix} from './vendor/ie-fix';
+import {vhFix} from './vendor/vh-fix';
+import {actualYear} from './modules/actualYear';
+import header from './components/header';
+import lazyLoading from './modules/lazyLoading';
+import scrollToAnchor from './modules/scrollToAnchor';
+
+ieFix();
+vhFix();
+actualYear();
+scrollToAnchor.init();
+
+header.init();
+lazyLoading.init();
+console.log(lazyLoading)
+$(".slideup").click(function(){
+    $('html, body').animate({
+        scrollTop:$(".home").position().top
+      }, 500);
+})
